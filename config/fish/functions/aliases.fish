a1db4af2fd2bf6ac6a383cc25d11ec199f272e52
Matthieu Mota <matthieumota@gmail.com>
2018-12-08 22:19:25 +0100

    [macOS] Add 7.3 to bx switcher php

--- config/fish/functions/aliases.fish
+++ config/fish/functions/aliases.fish
@@ -1,5 +1,5 @@
 function bx-switch-php --description "bx-switch-php <php-version>"
-  brew unlink php@5.6; brew unlink php@7.1; brew unlink php;
+  brew unlink php@5.6; brew unlink php@7.1; brew unlink php@7.2; brew unlink php;
 
   if [ "56" = "$argv[1]" ]
     brew link --force php@5.6
@@ -11,5 +11,11 @@ function bx-switch-php --description "bx-switch-php <php-version>"
     return 1
   end
 
+  if [ "72" = "$argv[1]" ]
+    brew link --force php@7.2
+    return 1
+  end
+
   brew link --force php
 end
+
