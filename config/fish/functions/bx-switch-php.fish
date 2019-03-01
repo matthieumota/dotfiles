34ef490d3e2495d2658b484401430930a26e515e
Matthieu Mota <matthieumota@gmail.com>
2019-03-01 07:57:19 +0100

    [Fish] Remove php 5.6 switch

--- config/fish/functions/bx-switch-php.fish
+++ config/fish/functions/bx-switch-php.fish
@@ -1,10 +1,5 @@
 function bx-switch-php --description "bx-switch-php <php-version>"
-  brew unlink php@5.6; brew unlink php@7.1; brew unlink php@7.2; brew unlink php;
-
-  if [ "56" = "$argv[1]" ]
-    brew link --force php@5.6
-    return 1
-  end
+  brew unlink php@7.1; brew unlink php@7.2; brew unlink php;
 
   if [ "71" = "$argv[1]" ]
     brew link --force php@7.1
