e12b3b7cd64992a52464d965884148ee212a0b64
Matthieu Mota <matthieumota@gmail.com>
2019-03-05 19:21:28 +0100

    [Fish] Update PATH for Arch

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,6 @@
 set PATH $HOME/.composer/vendor/bin $PATH
 set PATH /usr/local/opt/ruby/bin $PATH
 set PATH /usr/local/lib/ruby/gems/(ruby -e 'print Gem.ruby_api_version')/bin $PATH
+# Ruby is managed by user on Arch Linux, composer as well
+# It can be too on macOS but on native Ruby
+# set PATH (ruby -e 'print Gem.user_dir')/bin $PATH
