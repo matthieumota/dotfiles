9a0b8b26af23c250ea06df897f7976b35093d8dc
Matthieu Mota <matthieumota@gmail.com>
2019-12-03 22:37:39 +0100

    Remove php 7.1

--- config/fish/functions/bx-switch-php.fish
+++ config/fish/functions/bx-switch-php.fish
@@ -1,12 +1,12 @@
 function bx-switch-php --description "bx-switch-php <php-version>"
-  brew unlink php@7.1; brew unlink php@7.2; brew unlink php@7.3; brew unlink php;
+  brew unlink php@7.2; brew unlink php@7.3; brew unlink php;
 
-  if [ "71" = "$argv[1]" ]
-    brew link --force php@7.1
+  if [ "72" = "$argv[1]" ]
+    brew link --force php@7.2
     # Arch Linux
-    # sudo ln -sf /usr/bin/php71 /usr/local/bin/php
-    # sudo ln -sf /usr/bin/php-config71 /usr/local/bin/php-config
-    # sudo ln -sf /usr/bin/phpize71 /usr/local/bin/phpize
+    # sudo ln -sf /usr/bin/php72 /usr/local/bin/php
+    # sudo ln -sf /usr/bin/php-config72 /usr/local/bin/php-config
+    # sudo ln -sf /usr/bin/phpize72 /usr/local/bin/phpize
 
     # wget https://pecl.php.net/get/imagick-3.4.4.tgz
     # tar xf imagick-3.4.4.tgz
@@ -14,15 +14,7 @@ function bx-switch-php --description "bx-switch-php <php-version>"
     # phpize
     # ./configure --with-php-config=php-config
     # make
-    
-    return 0
-  end
 
-  if [ "72" = "$argv[1]" ]
-    brew link --force php@7.2
-    # sudo ln -sf /usr/bin/php72 /usr/local/bin/php
-    # sudo ln -sf /usr/bin/php-config72 /usr/local/bin/php-config
-    # sudo ln -sf /usr/bin/phpize72 /usr/local/bin/phpize
     return 0
   end
 
