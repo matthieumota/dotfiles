eca6148e6f7ba9fe08236529ae6cd51cc7f9f4c9
Matthieu Mota <matthieumota@gmail.com>
2021-04-19 23:07:12 +0200

    Amazing aliases

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,1 +1,24 @@
+# Misc
+alias c="clear"
+
+# Symfony
 alias sf="php bin/console"
+
+# Laravel
+alias artisan="php artisan"
+alias mf="php artisan migrate:fresh"
+alias mfs="php artisan migrate:fresh --seed"
+
+# JS
+alias watch="npm run watch"
+
+# PHP
+alias composer="php -d memory_limit=-1 /usr/local/bin/composer"
+
+# Git
+alias commit="git add . && git commit -m"
+alias gb="git branch"
+alias gc="git checkout"
+alias gl="git log --oneline"
+alias gst="git status"
+alias push="git push"
