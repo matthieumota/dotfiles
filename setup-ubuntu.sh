abe2d48e5b5706de72f021275d4afa687c652ebb
Matthieu Mota <matthieumota@gmail.com>
2023-12-18 22:32:16 +0100

    Fix ubuntu 22.04 installation

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -5,40 +5,39 @@ echo "Setting Linux..."
 export DEBIAN_FRONTEND=noninteractive
 
 # PPAs
-sudo apt-add-repository ppa:chris-lea/redis-server -y
 sudo apt-add-repository ppa:ondrej/apache2 -y
 sudo apt-add-repository ppa:ondrej/php -y
+sudo add-apt-repository ppa:serge-rider/dbeaver-ce -y
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo mkdir -p --mode=0755 /etc/apt/keyrings
+sudo mkdir -p --mode=0755 /usr/share/keyrings
 
-curl -sS https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+wget -O- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/linux_signing_key.pub
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
+sudo apt-key export D38B4796 | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/chrome.gpg
 echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-curl -sS https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
 echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" | sudo tee /etc/apt/sources.list.d/insomnia.list
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
-echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
 
-sudo apt-key adv --fetch-keys https://mariadb.org/mariadb_release_signing_key.asc
-echo "deb [arch=amd64,arm64,ppc64el,s390x] https://mirrors.xtom.nl/mariadb/repo/10.4/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/mariadb.list
-
-echo "deb [trusted=yes] https://repo.symfony.com/apt/ /" | sudo tee /etc/apt/sources.list.d/symfony-cli.list
-
-sudo mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared focal main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
 
 # Update
 sudo apt-get update
@@ -63,11 +62,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 curl -L https://get.oh-my.fish | fish /dev/stdin --noninteractive
 
 # Scaleway
-wget https://github.com/scaleway/scaleway-cli/releases/download/v1.20/scw_1.20_amd64.deb -O scw.deb
-sudo apt-get install -y ./scw.deb && rm scw.deb
+curl -s https://raw.githubusercontent.com/scaleway/scaleway-cli/master/scripts/get.sh | sh
 
 # Development
 sudo apt-get install -y \
+     php8.3-amqp php8.3-apcu php8.3-bcmath php8.3-cli php8.3-curl php8.3-fpm php8.3-gd php8.3-imagick php8.3-intl \
+     php8.3-mbstring php8.3-mysql php8.3-redis php8.3-soap php8.3-sqlite3 php8.3-xdebug php8.3-xml php8.3-zip \
      php8.2-amqp php8.2-apcu php8.2-bcmath php8.2-cli php8.2-curl php8.2-fpm php8.2-gd php8.2-imagick php8.2-intl \
      php8.2-mbstring php8.2-mysql php8.2-redis php8.2-soap php8.2-sqlite3 php8.2-xdebug php8.2-xml php8.2-zip \
      php8.1-amqp php8.1-apcu php8.1-bcmath php8.1-cli php8.1-curl php8.1-fpm php8.1-gd php8.1-imagick php8.1-intl \
@@ -129,8 +129,6 @@ sudo apt-get install -y ./discord.deb && rm discord.deb
 
 # Docker
 sudo apt-get install -y docker-ce
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $USER
 
 # Act
@@ -138,13 +136,13 @@ curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
 # PhpStorm
 if test ! $(which pstorm); then
-  wget https://download-cdn.jetbrains.com/webide/PhpStorm-2022.3.tar.gz
-  sudo tar xvf PhpStorm-2022.3.tar.gz --directory /opt/ && rm PhpStorm-2022.3.tar.gz
+  wget https://download.jetbrains.com/webide/PhpStorm-2023.3.1.tar.gz
+  sudo tar xvf PhpStorm-2023.3.1.tar.gz --directory /opt/ && rm PhpStorm-2023.3.1.tar.gz
 fi
 
 # Stripe
-wget https://github.com/stripe/stripe-cli/releases/download/v1.7.0/stripe_1.7.0_linux_x86_64.tar.gz
-sudo tar xvf stripe_1.7.0_linux_x86_64.tar.gz --directory /usr/local/bin/ && rm stripe_1.7.0_linux_x86_64.tar.gz
+wget https://github.com/stripe/stripe-cli/releases/download/v1.18.0/stripe_1.18.0_linux_x86_64.tar.gz
+sudo tar xvf stripe_1.18.0_linux_x86_64.tar.gz --directory /usr/local/bin/ && rm stripe_1.18.0_linux_x86_64.tar.gz
 
 # Postfix
 sudo debconf-set-selections <<< "postfix postfix/mailname string localhost"
