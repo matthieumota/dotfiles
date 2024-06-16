ada13ab3988ec5f84f7109a3385a648015aa0b13
Matthieu Mota <matthieumota@gmail.com>
2024-06-16 23:27:50 +0200

    wip arch

--- setup-arch.sh
+++ setup-arch.sh
@@ -12,15 +12,23 @@ sudo pacman -S \
      git \
      gnome-shell-extension-appindicator \
      nodejs \
+     npm \
      screenfetch \
+     ttf-dejavu \
      xorg-xdpyinfo \
      zsh
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Docker
-sudo usermod -aG docker $USER
+# Development
+sudo pacman -S \
+      composer \
+      php-apcu php-fpm \
+      nginx-mainline
+
+# NPM dependencies
+sudo npm install -g npm-check-updates
 
 # Apps
 sudo pacman -S \
@@ -29,14 +37,20 @@ sudo pacman -S \
      remmina \
      spotify-launcher
 
+# Docker
+sudo usermod -aG docker $USER
+
 # Aur
 yay -S \
+     google-chrome \
      powerline-fonts-git \
-     visual-studio-code-bin \
-     google-chrome
+     symfony-cli-bin \
+     visual-studio-code-bin
 
 yay -Yc
 
 # Dotfiles
 ln -s $HOME/.dotfiles/config/git/.gitconfig $HOME/.gitconfig
 ln -s $HOME/.dotfiles/config/git/.gitignore_global $HOME/.gitignore_global
+ln -s $HOME/.dotfiles/config/zsh/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh
+ln -s $HOME/.dotfiles/config/zsh/path.zsh $HOME/.oh-my-zsh/custom/path.zsh
