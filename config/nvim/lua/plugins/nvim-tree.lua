e7ecbd7802bfa465a26326ab26c79986dc0ec92d
Matthieu Mota <matthieumota@gmail.com>
2024-09-04 20:25:40 +0200

    Add nvim tree

---
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {},
+  keys = {
+    { "<leader>e", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Toggle NvimTree" },
+  },
+}
