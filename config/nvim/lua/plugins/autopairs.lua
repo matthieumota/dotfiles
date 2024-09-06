d7b5d12494fcc73b21bd875bc01593552ba5177c
Matthieu Mota <matthieumota@gmail.com>
2024-09-06 18:18:33 +0200

    Nvim lsp

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -1,4 +1,10 @@
 return {
   "windwp/nvim-autopairs",
-  opts = {},
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("nvim-autopairs").setup()
+    require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+  end,
 }
---
+++ config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,67 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-emoji",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local mason_lspconfig = require("mason-lspconfig")
+
+    require("mason").setup()
+
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "cssls",
+        "emmet_language_server",
+        "gopls",
+        "html",
+        "intelephense",
+        "tailwindcss",
+        "tsserver",
+        "volar",
+      },
+    })
+
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        server_name = server_name == "tsserver" and "ts_ls" or server_name
+        require("lspconfig")[server_name].setup({
+          capabilities = require("cmp_nvim_lsp").default_capabilities(),
+          root_dir = function(fname)
+            return vim.fn.getcwd()
+          end,
+        })
+      end,
+    })
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.confirm({ select = true })
+					else
+						fallback()
+					end
+				end),
+        ["<C-Space>"] = cmp.mapping(function(fallback)
+          if vim.lsp.buf.signature_help then
+            vim.lsp.buf.signature_help()
+          else
+            fallback()
+          end
+        end),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "emoji" },
+      }),
+    })
+  end,
+}
