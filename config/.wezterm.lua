942dec179dae90c90f4268cb91d3036f4a6e9724
Matthieu Mota <matthieumota@gmail.com>
2024-09-23 14:02:18 +0200

    fix wayland wezterm scale

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -5,6 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font("Operator Mono Lig Book")
 config.line_height = 1.2
+-- config.enable_wayland = false
 config.window_background_opacity = 0.9
 
 config.keys = {
