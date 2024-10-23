79e4a3dd4be412fd62ea2acc7666061af189f250
Matthieu Mota <matthieumota@gmail.com>
2024-10-23 18:41:35 +0200

    update nvim

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -6,7 +6,7 @@ config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font("Operator Mono Lig Book")
 config.line_height = 1.2
 -- config.enable_wayland = false
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.99
 
 config.keys = {
   {
