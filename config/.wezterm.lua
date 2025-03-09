14d004979e42fb97e93af5aec8f338a165f92fa4
Matthieu Mota <matthieumota@gmail.com>
2025-03-09 02:04:11 +0100

    enable wayland wezterm

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tokyo Night'
 -- config.font = wezterm.font("Operator Mono Lig Book")
 -- config.line_height = 1.2
--- config.enable_wayland = false
+config.enable_wayland = false
 config.window_background_opacity = 0.99
 
 config.keys = {
