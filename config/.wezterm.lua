021c5f8b057bbf3b93bde52caaf62802d73ba189
Matthieu Mota <matthieumota@gmail.com>
2025-03-21 20:26:57 +0100

    fix opacity wezterm

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -6,7 +6,7 @@ config.color_scheme = 'Tokyo Night'
 -- config.font = wezterm.font("Operator Mono Lig Book")
 -- config.line_height = 1.2
 config.enable_wayland = false
-config.window_background_opacity = 0.99
+config.window_background_opacity = 0.95
 
 config.keys = {
   {
