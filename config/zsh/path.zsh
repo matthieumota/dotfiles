c0fe601412cd23a48a1d494704f2c2fa5b46d318
Matthieu Mota <matthieumota@gmail.com>
2025-08-20 17:23:55 +0200

    add path mac

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -1,3 +1,4 @@
 export PATH=$HOME/.config/composer/vendor/bin:$PATH
 # export PATH=$HOME/.composer/vendor/bin:$PATH
 # export PATH="/opt/homebrew/opt/rustup/bin:$PATH"
+# export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
