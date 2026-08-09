3d625bbd0dfd9dd2f92a7c5db1dc3248cd42b465
Matthieu Mota <matthieumota@gmail.com>
2026-08-09 08:52:09 +0200

    add go bin to path

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -2,5 +2,6 @@ export PATH=$HOME/.config/composer/vendor/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.composer/vendor/bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/go/bin:$PATH
 export PATH=/opt/homebrew/opt/rustup/bin:$PATH
 export PATH=/opt/homebrew/opt/libpq/bin:$PATH
