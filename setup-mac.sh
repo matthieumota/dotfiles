cad959b5f6007807ffa4774191dc82d052d22dc4
Matthieu Mota <matthieumota@gmail.com>
2026-08-18 22:10:16 +0200

    fix non interactive codex

--- setup-mac.sh
+++ setup-mac.sh
@@ -49,7 +49,7 @@ nvm install node
 npm install -g npm-check-updates
 curl -fsSL https://gh.io/copilot-install | bash
 curl -fsSL https://opencode.ai/install | bash
-curl -fsSL https://chatgpt.com/codex/install.sh | sh
+curl -fsSL https://chatgpt.com/codex/install.sh | CODEX_NON_INTERACTIVE=1 sh
 curl -fsSL https://claude.ai/install.sh | bash
 
 # Bun
