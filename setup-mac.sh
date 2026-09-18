e6d94a2d73c1237e09be0dc15a17e3b3cede6187
Matthieu Mota <matthieumota@gmail.com>
2026-09-18 23:38:36 +0200

    replace which

--- setup-mac.sh
+++ setup-mac.sh
@@ -2,7 +2,7 @@
 
 echo "Setting Mac..."
 
-if test ! $(which brew); then
+if ! command -v brew >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   eval "$(/opt/homebrew/bin/brew shellenv)"
